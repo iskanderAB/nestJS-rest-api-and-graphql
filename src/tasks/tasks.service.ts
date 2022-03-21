e5ccfa65df8c07f91f1c2eb0cca53fcda88474b1
iskanderAB <iskanderabasi@gmail.com>
2022-03-21 23:07:12 +0100

    add api

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks:Task[] = [
+        {
+            id: '10',
+            description: 'hello iskander',
+            title: 'iskander',
+            status: TaskStatus.OPEN
+        },
+        {
+            id: '10',
+            description: 'hello iskander',
+            title: 'iskander',
+            status: TaskStatus.OPEN
+        },
+        {
+            id: '10',
+            description: 'hello iskander',
+            title: 'iskander',
+            status: TaskStatus.OPEN
+        }
+    ]
+    getAllTasks(){
+        return this.tasks;
+    }
+
+}
